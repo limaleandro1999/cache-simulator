9485a3bd6ce43c650b9aa3999afcfb0b5d827678
limaleandro1999 <limaleandro1999@gmail.com>
2019-03-12 22:58:59 +0000

    alterações até agora

--- index.js
+++ index.js
@@ -1,163 +1,76 @@
 const fs = require('fs');
 const readline = require('readline');
 
-const cache1024 = {cache: new Array(64), cacheMiss: 0, cacheHit: 0, length: 1024};
-const cache2Way1024 = {cache: [new Array(32), new Array(32)], cacheMiss: 0, cacheHit: 0, length: 1024};
+const cacheSize = 1;
+const cacheLineSize = 16;
+const cacheSetLines = 4;
+const memoryAddressSize = 32;
+const cacheReplacePolicy = 0;
+const cacheSizeUnit = 10;
 
-const cache2048 = {cache: new Array(128), cacheMiss: 0, cacheHit: 0, length: 2048};
-const cache2Way2048 = {cache: [new Array(64), new Array(64)], cacheMiss: 0, cacheHit: 0, length: 2048};
+console.log(`Cache Size ${cacheSize} KB, Cache Block Size ${cacheLineSize}`);
 
-const cache4096 = {cache: new Array(256), cacheMiss: 0, cacheHit: 0, length: 4096};
-const cache2Way4096 = {cache: [new Array(128), new Array(128)], cacheMiss: 0, cacheHit: 0, length: 4096};
+const numberCacheLines = (Math.pow(2 * cacheSize, cacheSizeUnit)) / cacheLineSize;
+const cacheSetSize = cacheLineSize * cacheSetLines;
 
-const cache8192 = {cache: new Array(512), cacheMiss: 0, cacheHit: 0, length: 8192};
-const cache2Way8192 = {cache: [new Array(256), new Array(256)], cacheMiss: 0, cacheHit: 0, length: 8192};
+const offsetBits = Math.log2(cacheLineSize);
+const indexBits = Math.log2(((Math.pow(2 * cacheSize, cacheSizeUnit)) / cacheSetSize));
+const tagBits = memoryAddressSize - offsetBits - indexBits;
 
-const cache16384 = {cache: new Array(1024), cacheMiss: 0, cacheHit: 0, length: 16384};
-const cache2Way16384 = {cache: [new Array(512), new Array(512)], cacheMiss: 0, cacheHit: 0, length: 16384};
+console.log(`Cache format, tag bits ${tagBits}, index bits ${indexBits}, offset bits ${offsetBits}`)
 
-await cache2WayInit([cache2Way1024, cache2Way2048, cache2Way4096, cache2Way8192, cache2Way16384]);
+let cacheHit = 0;
+let cacheMiss = 0;
+let time_stamp = 0;
+let memReq = 0;
+
+const cacheLine = { valid: 0, tag: 0, lastAccess: 0, numberAccess: 0 };
+const cache = new Array(numberCacheLines);
+
+console.log(numberCacheLines);
+
+cache.fill(cacheLine);
 
 const rd = readline.createInterface({
   input: fs.createReadStream('./trace'),
   console: false
 });
 
-console.log('Please wait, i am simulating cache');
-
 rd.on('line', function(line) {
-  cacheCheck(line, cache1024);
-  cacheCheck(line, cache2048);
-  cacheCheck(line, cache4096);
-  cacheCheck(line, cache8192);
-  cacheCheck(line, cache16384);
+  memReq++;
+  const address = convertNumber(line, 16);
+  
+  let offset = address&((Math.pow(2, offsetBits) -1));
+  let index = (address >> offsetBits) & (Math.pow(2, indexBits) -1);
+  let tag = (address >> (offsetBits + indexBits)) & (Math.pow(2, tagBits) -1);
 
-  cacheSetAssociativeCheckFIFO(line, cache2Way1024);
-  cacheSetAssociativeCheckFIFO(line, cache2Way2048);
-  cacheSetAssociativeCheckFIFO(line, cache2Way4096);
-  cacheSetAssociativeCheckFIFO(line, cache2Way8192);
-  cacheSetAssociativeCheckFIFO(line, cache2Way16384);
+  let miss = 1;
+
+  for(let i = index; i < (index + cacheSetLines); i++){
+    if(cache[i].tag === tag){
+      cacheHit++;
+      miss = 0;
+      break;
+    }
+  }
+
+  if(miss > 0){
+    cacheMiss++;
+
+    for(let i = (index * 4); i < ((index * 4) + cacheSetLines); i++){
+      if(cache[i].valid === 0){
+        cache[i].valid = 1;
+        cache[i].tag = tag;
+        break;
+      }
+    } 
+  }
 });
 
 rd.on('close', () => {
-  console.log(`         ${cache1024.length}     ${cache2048.length}      ${cache4096.length}     ${cache8192.length}     ${cache16384.length}`);
-  console.log(`Direto   ${cache1024.cacheMiss}   ${cache2048.cacheMiss}   ${cache4096.cacheMiss}   ${cache8192.cacheMiss}   ${cache16384.cacheMiss}`);
+  console.log(cache);
 });
 
 const convertNumber = (n, fromBase, toBase = 10) => {
   return parseInt(n.toString(), fromBase).toString(toBase);
-};
-
-const cacheCheck = (line, cache) => {
-  let start
-  
-  switch(cache.length){
-    case 1024:
-      start = 21;
-      break;
-    case 2048:
-      start = 20;
-      break;
-    case 4096:
-      start = 19;
-      break;
-    case 8192:
-      start = 18;
-      break;
-    case 16384:
-      start = 17;
-      break;
-  }
-
-  let binaryAddr = convertNumber(line, 16, 2);
-  let cacheIndex = getCacheIndex(binaryAddr, start);
-
-  if(!cache.cache[cacheIndex] || cache.cache[cacheIndex] !== binaryAddr){
-    cache.cache[cacheIndex] = binaryAddr;
-    cache.cacheMiss++;
-  }else{
-    cache.cacheHit++;
-  }
-};
-
-const cache2WayInit = (caches2Way = []) => {
-  return new Promise((resolve, reject) => {
-    caches2Way.map(item => {
-      item.cache[0].map(cacheItem => {
-        cacheItem.count = 0;
-        cacheItem.addr = undefined;
-      })
-  
-      item.cache[1].map(cacheItem => {
-        cacheItem.count = 0;
-        cacheItem.addr = undefined;
-      })
-    })
-
-    resolve(true);
-  });
-};
-
-const cacheSetAssociativeCheckFIFO = (line, cache, numberOfSets) => {
-  let start
-  
-  switch(cache.length){
-    case 1024:
-      start = 21;
-      break;
-    case 2048:
-      start = 20;
-      break;
-    case 4096:
-      start = 19;
-      break;
-    case 8192:
-      start = 18;
-      break;
-    case 16384:
-      start = 17;
-      break;
-  }
-
-  let binaryAddr = convertNumber(line, 16, 2);
-  let cacheIndex = getCacheIndex(binaryAddr, start);
-
-  const set = cacheIndex % numberOfSets; 
-  let count = 0;
-
-  cache.cache2Way[set].map(values => {
-    if(values.addr === binaryAddr){
-      count ++;
-    }
-  });
-
-  if(count === 0){
-    cache.cacheMiss ++;
-
-    const blankIndex = cache.cache2Way[set].findIndex(item => !item.addr);
-
-    if(blankIndex > -1){
-      cache.cache2Way[set][blankIndex].addr = binaryAddr;
-      cache.cache2Way[set][blankIndex].count = 0;
-    }else{
-      let greatCount = -1;
-      let firstInIndex = -1;
-
-      cache.cache2Way[set].map((values, index) => {
-        if(values.count > greatCount){
-          firstInIndex = index;
-        }
-      });
-
-      cache.cache2Way[set][firstInIndex].addr = binaryAddr;
-      cache.cache2Way[set][firstInIndex].count = 0;
-    }
-  }
-};
-
-const getCacheIndex = (binaryAddr, start) => {
-  let cacheIndexBinary = binaryAddr.slice(start, 27);
-  let cacheIndex = convertNumber(cacheIndexBinary, 2);
-
-  return cacheIndex;
 };
